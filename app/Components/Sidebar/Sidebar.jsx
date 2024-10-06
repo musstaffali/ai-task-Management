8d16a1415c025466f2f1859ff2e88d9cbe86581a
Mustafa <mamusstaffali@gmail.com>
2024-10-06 16:12:57 -0400

    layout.tsx

--- app/Components/Sidebar/Sidebar.jsx
+++ app/Components/Sidebar/Sidebar.jsx
@@ -1,10 +1,10 @@
+"use client";
 import React from "react";
 
 function Sidebar() {
     return (
-        <div>
-            <h1>Sidebar</h1>
-        </div>
+        <div>Sidebar</div>
+       
     )
 }
 
